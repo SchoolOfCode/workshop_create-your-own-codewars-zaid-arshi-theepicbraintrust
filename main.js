80e97f6ce459f8d42306054b1b2b2eca9c550e54
crayola-eater <a-sheikh@outlook.com>
2020-10-19 15:49:04 +0100

    Tidied up wording

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 /* 
-👉 Write your kata here!
+Write a function that takes ANY number of objects and returns ONE object which contains totals for all encountered keys and values.
 
-Write a function that takes ANY number of objects (below is just an example):
-[
+So, for example, if we handed the function the objects below:
+
     {a: 1, b: 2},
     {a: 2, b: 7, c: 3},
-    {a: 5,  c: 3}
-]
+    {a: 5, c: 3}
 
-and returns ONE object which contains totals for all encountered keys and values:
-{a: 8, b: 9, c: 6}
+we'd expect back:
 
-Remember that each object in the array may not have the same properties (as the other objects).
-Each object (in the input array) will not have any nested objects as values.
+  {a: 8, b: 9, c: 6}
+
+Remember that each object in the array may (or may not) have the same properties (as the other objects).
+The object will not have any nested objects as values (so you don't have to worry about arbitrarily deep/nested objects).
+You can assume that your function will only receive objects (so you don't need to write guards/checks for non-objects).
 
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
-
 function addObjects(...objects) {
-  // Write your code here.
+  // Write your code here, good luck, thanks!
 }
 
 module.exports = {
